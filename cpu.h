6a6a304ca3c45e4bca6235afa9844e4b1be2e8a9
KevinZZhu <zhuk@oregonstate.edu>
2021-04-26 15:36:37 -0700

    moved state prototypes to cpu.h, and moved typedef of state structs to cpu.c

---
+++ cpu.h
@@ -0,0 +1,9 @@
+/****************************************************************************************
+** File Name: cpu.h
+** Authors: Kevin Zhu
+** Description: This file contains prototypes for structs that define the state of the
+** simulated 8080 cpu.
+****************************************************************************************/
+
+typedef struct ConditionCodes;
+typedef struct State8080;
