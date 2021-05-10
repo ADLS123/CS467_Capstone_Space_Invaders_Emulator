257e121e1ebd80348f301c6e2ade5304111ecd0f
KevinZZhu <zhuk@oregonstate.edu>
2021-05-10 13:41:22 -0700

    Added ioPorts file and header to handle IN and OUT opcodes

---
+++ ioPorts.c
@@ -0,0 +1,20 @@
+/**************************************************************************************************
+** File Name: ioPorts.c
+** Description: This file contains function declarations for handling the calls to the IO ports.
+**************************************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "ioPorts.h"
+
+
+uint8_t machineIN(state8080* state, uint8_t port) {
+	uint8_t a = 0;
+	switch (port) {
+		case 3:
+		{
+			uint16_t = 
+		}
+	}
+}
---
+++ ioPorts.h
@@ -0,0 +1,10 @@
+/**************************************************************************************************
+** File Name: ioPorts.h
+** Description: This file contains function prototypes for handling the calls to the IO ports.
+**************************************************************************************************/
+
+#include "cpu.h"
+
+#ifndef IOPORTS_H
+#define IOPORTS_H
+
