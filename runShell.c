a3b9542a4c88b4851ccc5c0502efc2ee73ee0b48
KevinZZhu <zhuk@oregonstate.edu>
2021-05-10 14:54:30 -0700

    Added machine IO code handling to runShell.c

--- runShell.c
+++ runShell.c
@@ -12,6 +12,7 @@
 
 #include "helperFunctions.h"
 #include "cpu.h"
+#include "ioPorts.h"
 
 
 // Global variable for keeping time
@@ -20,6 +21,7 @@ long lastInterrupt = 0;
 
 int main(int argc, char** argv) {
 	State8080* state = init8080CPU();
+	StateSIMachine* machine = initSIMachine();
 	readInvaderstoMem(state);
 	
 	
@@ -27,12 +29,27 @@ int main(int argc, char** argv) {
 
 	while (1) {
 		
-		if(getchar());
-		{emulate8080(state);
-		extractOpCode(state->memory, state->pc);
-		printf("OP Current: %x\nPC: %x\nSP: %x\n",state->memory[state->pc],state->pc, state->sp);
-		printf("Registers:  A: %x B: %x C: %x D: %x E: %x H: %x L: %x \n",state->a,state->b,state->c,state->d,state->e,state->h,state->l);
-		printf("---------------------\n");
+		if(getchar()) {
+
+			uint8_t* opCode = state->memory[state->pc];
+
+			if (*opCode == 0xdb) {	// machine IN
+				machineIN(state, machine, opCode[1]);
+				extractOpCode(state->memory, state->pc);
+				state->pc++;
+			}
+			else if (*opCode == 0xd3) { // machine OUT
+				machineOUT(state, machine, opCode[1]);
+				extractOpCode(state->memory, state->pc);
+				state->pc++;
+			}
+			else { // regular 8080 opcodes
+				emulate8080(state);
+				extractOpCode(state->memory, state->pc);
+				printf("OP Current: %x\nPC: %x\nSP: %x\n", state->memory[state->pc], state->pc, state->sp);
+				printf("Registers:  A: %x B: %x C: %x D: %x E: %x H: %x L: %x \n", state->a, state->b, state->c, state->d, state->e, state->h, state->l);
+				printf("---------------------\n");
+			}
 		}
 
 		// trigger interrupt if 1/60 of a second has passed since last interrupt, and interrupts are enabled
